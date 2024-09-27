1e738d68c775e6c871cb5a36326e1373dfde2743
PedrinhoAS <62121621+pedrinhoas@users.noreply.github.com>
2024-09-27 10:51:55 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:planetas_app/screens/tela_inicial.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const TelaInicial(),
+    );
+  }
+}
